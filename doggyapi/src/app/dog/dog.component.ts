1b8dbf0628857c0dd8dc47c4793600bffb80b9fa
unknown <mirandas.angelica@gmail.com>
2017-09-13 12:22:31 -0500

    doggyapi

---
+++ doggyapi/src/app/dog/dog.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Http, Response } from '@angular/http';
+
+@Component({
+  selector: 'app-dog',
+  templateUrl: './dog.component.html',
+  styleUrls: ['./dog.component.styl']
+})
+export class DogComponent implements OnInit {
+
+  breed: string;
+  allPictures: any;
+
+  constructor(private route: ActivatedRoute, private http: Http) { }
+
+  getDog(raza){
+    this.http.get("https://dog.ceo/api/breed/"+ raza + "/images").subscribe((res: Response) => this.allPictures = res.json());
+  }
+  
+  ngOnInit() {
+    this.route.params.subscribe(params => {this.breed = params['breed']});
+    this.getDog(this.breed);
+  }
+
+}
