0d3908396b72e67326eb28199018e8c53643bcfb
unknown <mirandas.angelica@gmail.com>
2017-11-02 15:06:53 -0600

    add: graph in details component

--- bookstore/src/app/details/details.component.ts
+++ bookstore/src/app/details/details.component.ts
@@ -20,30 +20,37 @@ export class DetailsComponent implements OnInit {
       this.book = data.filter((el) => { 
         this.route.params.subscribe(params=> {this.bookId = params['id']});
         return el.id == this.bookId})[0];
-        console.log(this.book)
         this.ratings.push(100 * this.book.ratings_1 / this.book.work_ratings_count);
         this.ratings.push(100 * this.book.ratings_2 / this.book.work_ratings_count);
         this.ratings.push(100 * this.book.ratings_3 / this.book.work_ratings_count);
         this.ratings.push(100 * this.book.ratings_4 / this.book.work_ratings_count);
         this.ratings.push(100 * this.book.ratings_5 / this.book.work_ratings_count);
+        d3.select('#graph')
+          .selectAll('rect')
+          .data(this.ratings)
+          .enter()
+          .append('rect')
+          .attr('x',(d, i)=>{
+            return i * 50;
+          })
+          .attr('y',(d, i) => {
+            return 210 - d*2 
+          })
+          .attr('width', '30')
+          .attr('height', function(d){
+            return d*2;
+          })
+          .attr('fill', '#F3DF2C')
+          .append('text')
+          .attr('x', (d,i) => {
+            return i * 50 + 5
+          })
+          .attr('y', '210')
+          .text((d, i) => {return i})
+                  
     });
 
-    d3.select('#graph')
-      .selectAll('rect')
-      .data(this.ratings)
-      .enter()
-      .append('rect')
-      .attr('x',(d, i)=>{
-        return i * 50;
-      })
-      .attr('y',(d, i) => {
-        return 100 - d 
-      })
-      .attr('width', '30')
-      .attr('height', function(d){
-        return d;
-      })
-      .attr('fill', '#cdcdcd')
+    
   }
 
 }
